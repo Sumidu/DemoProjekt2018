0ea4d51bdf73c6766a40dfd82b29cf3fa0e18366
André Calero Valdez <andrecalerovaldez@gmail.com>
2018-11-20 15:39:40 +0100

    Updated install_libraries.R

--- install_libraries.R
+++ install_libraries.R
@@ -3,3 +3,13 @@
 install.packages("tidyverse")
 install.packages("lubridate")
 install.packages("psych")
+install.packages("esquisse")
+install.packages("ggthemes")
+
+
+install.packages("devtools")
+library(devtools)
+devtools::install_github("HCIC/r-tools")
+
+
+
